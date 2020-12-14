d4c1098356b8bce05a309394f754eef7557eef4d
ChadSutherland <csuth1@live.com>
2020-12-14 15:18:21 -0500

    Add label to filter combobox, switch to vertical drag knobs

--- Source/Filter.cpp
+++ Source/Filter.cpp
@@ -16,7 +16,7 @@ Filter::Filter(BasicSynthAudioProcessor& p) :
     processor(p)//, envModel(0,0,0,0)
 {
     setOpaque (true);
-    setSize(240, 200);
+    setSize(240, 160);
     
     //filtermenu config
     filterMenu.addItem("Low Pass", 1);
@@ -24,12 +24,16 @@ Filter::Filter(BasicSynthAudioProcessor& p) :
     filterMenu.addItem("Band Pass", 3);
     filterMenu.setJustificationType(Justification::centred);
     filterMenu.setSelectedId(1);
+    filterLabel.setText ("filter", dontSendNotification);
+    filterLabel.setFont(Font(16));
+    filterLabel.setJustificationType(Justification::left);
+    filterLabel.attachToComponent(&filterMenu, true);
     addAndMakeVisible(&filterMenu);
-    filterMenuAttachment = std::make_unique<AudioProcessorValueTreeState::ComboBoxAttachment>(processor.tree, "filtertype", filterMenu);
+    filterMenuAttachment = std::make_unique<AudioProcessorValueTreeState::ComboBoxAttachment>(processor.tree, "filterType", filterMenu);
     
     //cutoffKnob config
     cutoffKnob.setLookAndFeel(&customLookAndFeel);
-    cutoffKnob.setSliderStyle(Slider::Rotary);
+    cutoffKnob.setSliderStyle(Slider::RotaryVerticalDrag);
     cutoffKnob.setTextBoxStyle(Slider::TextBoxBelow, false, 50, 20);
     cutoffKnob.setSize(getWidth()/4,ctrlBoxHeight-border);
     cutoffLabel.setText ("Cutoff", dontSendNotification);
@@ -38,12 +42,12 @@ Filter::Filter(BasicSynthAudioProcessor& p) :
     cutoffLabel.attachToComponent(&cutoffKnob, false);
     addAndMakeVisible(&cutoffKnob);
     cutoffKnob.addListener(this);
-    cutoffAttachment = std::make_unique<AudioProcessorValueTreeState::SliderAttachment>(processor.tree, "cutoff", cutoffKnob);
+    cutoffAttachment = std::make_unique<AudioProcessorValueTreeState::SliderAttachment>(processor.tree, "filterCutoff", cutoffKnob);
     cutoffKnob.setNumDecimalPlacesToDisplay(2);
-    
+
     //resKnob config
     resKnob.setLookAndFeel(&customLookAndFeel);
-    resKnob.setSliderStyle(Slider::Rotary);
+    resKnob.setSliderStyle(Slider::RotaryVerticalDrag);
     resKnob.setTextBoxStyle(Slider::TextBoxBelow, false, 50, 20);
     resKnob.setSize(getWidth()/4,ctrlBoxHeight-border);
     resLabel.setText ("Res", dontSendNotification);
@@ -52,13 +56,8 @@ Filter::Filter(BasicSynthAudioProcessor& p) :
     resLabel.attachToComponent(&resKnob, false);
     addAndMakeVisible(&resKnob);
     resKnob.addListener(this);
-    resAttachment = std::make_unique<AudioProcessorValueTreeState::SliderAttachment>(processor.tree, "res", resKnob);
+    resAttachment = std::make_unique<AudioProcessorValueTreeState::SliderAttachment>(processor.tree, "filterRes", resKnob);
     resKnob.setNumDecimalPlacesToDisplay(2);
-    
-
-//    envModel.updateNodes(getAttack(), getDecay(), getSustain(), getRelease());
-//    addAndMakeVisible(&envModel);
-    
 }
 
 Filter::~Filter()
@@ -92,24 +91,15 @@ void Filter::paint (Graphics& g)
 
 void Filter::resized()
 {
-    
-//    Rectangle<int> area = getLocalBounds().reduced(border);
-//    int knobWidth = area.getWidth() / 4;
-//
-//    envModel.setBounds(area.removeFromTop(envModel.getHeight()));
-//    area.removeFromTop(20);
-//    attackKnob.setBounds(area.removeFromLeft(knobWidth));
-//    decayKnob.setBounds(area.removeFromLeft(knobWidth));
-//    sustainKnob.setBounds(area.removeFromLeft(knobWidth));
-//    releaseKnob.setBounds(area.removeFromLeft(knobWidth));
 
     Rectangle<int> area = getLocalBounds().reduced(border);
-    int childWidth = area.getWidth() / 3;
+    int childWidth = area.getWidth() / 2;
 
-    filterMenu.setBounds(area.removeFromTop(20));
-    area.removeFromTop(10);
-    cutoffKnob.setBounds(area.removeFromLeft(childWidth));
-    resKnob.setBounds(area.removeFromLeft(childWidth));
+    filterMenu.setBounds(area.removeFromTop(25).removeFromRight(180));
+    area.removeFromTop(30);
+
+    cutoffKnob.setBounds(area.removeFromLeft(childWidth).removeFromRight(childWidth-30));
+    resKnob.setBounds(area.removeFromLeft(childWidth-30));
 }
 
 void Filter::sliderValueChanged(Slider* slider)
